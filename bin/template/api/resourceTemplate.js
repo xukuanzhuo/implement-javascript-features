2cb60d7528c2bf08191b352c4c730ba19c7b8ea0
xkz <1060821554@qq.com>
2018-12-14 15:27:35 +0800

    create api template

---
+++ bin/template/api/resourceTemplate.js
@@ -0,0 +1,6 @@
+function firstUpperCase(str) {
+  return str.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
+}
+module.exports = function(moduleName, filename){
+  return `import request from '@/admin/utils/request'\n\nexport function get${firstUpperCase(filename)}s (filterQuery) {\n  return request({\n    url: 'api/${moduleName}/${filename}s',\n    method: 'get',\n    params: filterQuery\n  })\n}\n\nexport function create${firstUpperCase(filename)} (${filename}Params) {\n  return request({\n    url: 'api/${moduleName}/${filename}s',\n    method: 'post',\n    params: ${filename}Params\n  })\n}\n\nexport function update${firstUpperCase(filename)} (${filename}Params) {\n  return request({\n    url: 'api/${moduleName}/${filename}s/`+'${'+`${filename}Params`+'.id}'+ `,\n    method: 'put',\n    params: ${filename}Params\n  })\n}\n\nexport function delete${firstUpperCase(filename)} (id) {\n  return request({\n    url: api/${moduleName}/${filename}s/`+'${'+'id'+'}'+`,\n    method: 'delete'\n  })\n}`
+}
---
+++ bin/template/views/indexCssTemplate.js
@@ -0,0 +1,3 @@
+module.exports = function(filename){
+  return `.header {\n  margin-bottom: 20px;\n\n  .header-actions {\n    display: inline-block;\n    float: right;\n  }\n}`
+}
